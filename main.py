ba828475cb60b41a0fbe5ee14f7f38dc8768aa56
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-08-17 21:09:43 +0800

    add estimated distance

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ Created on Sat Aug 15 22:07:55 2020
 
 import numpy as np
 import cv2
+from math import sqrt, pow
 
+def getDistance(point1, point2):
+    diffX = point1[0] - point2[0]
+    diffY = point1[1] - point2[1]
+    
+    return sqrt(pow(diffX,2) + pow(diffY,2))
+    
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -23,9 +30,27 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        cx_list = []
+        cy_list = []
+        
         for (ex,ey,ew,eh) in eyes:
+            cx_list.append(int(ex+ew/2))
+            cy_list.append(int(ey+eh/2))
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
+            
+        if (len(cx_list) == 2):
+            point1 = (cx_list[0], cy_list[0])
+            point2 = (cx_list[1], cy_list[1])
+            cv2.line(roi_color, point1, point2, (0,0,255),2)
+            
+            pixelDistance = getDistance(point1, point2)
+            
+            #the estimated distance is obtained from measurement and interpolation
+            estimatedDistance = -0.7249*pixelDistance + 103.85
+            
+            print(str(estimatedDistance) + " cm")
+            
     cv2.imshow('img',frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
